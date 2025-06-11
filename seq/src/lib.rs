40040977c4d49a0cb225dbed2332bbbb139bc0c3
Timm Behner <timm.behner@gmail.com>
2025-06-11 16:03:55 +0200

    stage 5 of seq works

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,17 @@
 use itertools::{Itertools, MultiPeek};
-use proc_macro2::{token_stream, Group, Span, TokenStream, TokenTree};
+use proc_macro2::{token_stream, Delimiter, Group, TokenStream, TokenTree};
 use quote::quote;
 use std::ops::Range;
-use syn::{braced, parse::Parse, parse_macro_input, parse_quote, visit_mut, visit_mut::VisitMut, Expr, ExprRange, Ident, Lit, LitInt, Token};
+use syn::{braced, parse::Parse, parse_macro_input, Expr, ExprRange, Ident, Lit, Token};
+
+fn get_range(input: &ExprRange) -> Range<u16> {
+    let start_expr = *input.start.clone().unwrap();
+    let start = get_limit(start_expr);
+    let end_expr = *input.end.clone().unwrap();
+    let end = get_limit(end_expr);
+    start..end
+}
+
 
 #[derive(Debug)]
 struct Seq {
@@ -11,12 +20,10 @@ struct Seq {
     tokens: proc_macro2::TokenStream,
 }
 
-fn get_range(input: &ExprRange) -> Range<u16> {
-    let start_expr = *input.start.clone().unwrap();
-    let start = get_limit(start_expr);
-    let end_expr = *input.end.clone().unwrap();
-    let end = get_limit(end_expr);
-    start..end
+fn clone_group_with_stream(old_group: &Group, stream: TokenStream) -> Group {
+    let mut new_group = Group::new(old_group.delimiter(), stream);
+    new_group.set_span(old_group.span());
+    new_group
 }
 
 impl Seq {
@@ -51,13 +58,11 @@ impl Seq {
                     if ident.to_string() == self.ident.to_string() {
                         let num_word = format!("{}", num);
                         let num_tokens: TokenStream = num_word.parse().unwrap();
-                        let num_stream = quote! {#num_tokens};
-                        out.extend(num_stream.into_iter());
+                        out.extend(num_tokens.into_iter());
                     } else if self.peek_is_tilde_token(&mut iter) && self.peek_is_ident(&mut iter) {
                         let num_word = format!("{}{}", ident, num);
                         let num_tokens: TokenStream = num_word.parse().unwrap();
-                        let num_stream = quote! {#num_tokens};
-                        out.extend(num_stream.into_iter());
+                        out.extend(num_tokens.into_iter());
                         // since we have found the identifier, we have to extend the iter twice
                         for _ in 0..2 {
                             iter.next();
@@ -84,11 +89,106 @@ impl Seq {
         TokenStream::from_iter(out)
     }
 
-    fn quote(&self) -> TokenStream {
+    fn next_is_parenthesis_group(&self, iter: &mut MultiPeek<token_stream::IntoIter>) -> bool {
+        if let Some(TokenTree::Group(gr)) = iter.peek() {
+            gr.delimiter() == Delimiter::Parenthesis
+        } else { false }
+    }
+
+    fn next_is_punct_star(&self, iter: &mut MultiPeek<token_stream::IntoIter>) -> bool {
+        if let Some(TokenTree::Punct(p)) = iter.peek() {
+            p.as_char() == '*'
+        } else {false}
+    }
+
+    fn extract_repeat_section(&self, iter: &mut MultiPeek<token_stream::IntoIter>) -> TokenStream {
+        if let Some(TokenTree::Group(gr)) = iter.next() {
+            gr.stream().clone()
+        } else {
+            panic!("You have to identify next token tree as a group first!");
+        }
+    }
+
+    fn repeat_section(&self, section: TokenStream) -> TokenStream {
         let all: Vec<_> = self.get_range().map(|n| {
-             self.subs(&self.tokens, n)
+            self.subs(&section, n)
         }).collect();
-        quote!{#(#all)*}
+        quote! {#(#all)*}
+    }
+
+    fn identify_repeat_section(&self, stream: &TokenStream) -> (Option<TokenStream>, TokenStream, Option<TokenStream>) {
+        // find a Punct(#), followed by a Group with paranthesis as delimiter, followed by a
+        // Punct(*)
+        let mut pre = vec![];
+        let mut repeat_section = None;
+        let mut post = vec![];
+        let mut iter = stream.clone().into_iter().multipeek();
+
+        while let Some(t) = iter.next() {
+            match &t {
+                tt_punct@TokenTree::Punct(c) if c.as_char() == '#' => {
+                    if self.next_is_parenthesis_group(&mut iter) && self.next_is_punct_star(&mut iter) {
+                        // repeat this section
+                        repeat_section = Some(self.repeat_section(self.extract_repeat_section(&mut iter)));
+                        // also skip the '*'
+                        iter.next();
+                    } else {
+                        // not it
+                        pre.push(tt_punct.clone());
+                    }
+                }
+
+                token_tree_group@TokenTree::Group(ref gr) => {
+                    // if we have *not* found a section yet, descend into the group
+                    if repeat_section.is_none() {
+                        let (maybe_pre, sec, maybe_post) = self.identify_repeat_section(&gr.stream());
+                        // if the pre and post are not None: add the pre to pre, the post to post and set the section
+                        if let (Some(pre_sec), Some(post_sec)) = (maybe_pre, maybe_post) {
+                            // repeat sec here then create group with [pre, #(sec)*, post] as stream
+
+                            let mut complete = vec![];
+                            complete.extend(pre_sec);
+                            complete.extend(sec);
+                            complete.extend(post_sec);
+
+                            let group_section = clone_group_with_stream(gr, TokenStream::from_iter(complete));
+
+                            repeat_section = Some(TokenStream::from_iter(vec![TokenTree::Group(group_section)]));
+                        } else {
+                            // push the entire group to pre
+                            pre.push(token_tree_group.clone());
+                        }
+                    } else {
+                        // else add the group to post
+                        post.push(token_tree_group.clone());
+                    }
+                }
+                _ => {
+                    if repeat_section.is_none() {
+                        pre.push(t);
+                    } else {
+                        post.push(t);
+                    }
+                },
+            }
+        }
+
+        if let Some(ts) = repeat_section {
+            let pre_token_stream = TokenStream::from_iter(pre);
+            let post_token_stream = TokenStream::from_iter(post);
+            (Some(pre_token_stream), ts, Some(post_token_stream))
+        } else {
+            (None, self.tokens.clone(), None)
+        }
+    }
+
+    fn quote(&self) -> TokenStream {
+        let (maybe_pre, repeat_section , maybe_post)= self.identify_repeat_section(&self.tokens);
+        if let (Some(pre), Some(post)) = (maybe_pre, maybe_post) {
+            quote!{#pre #repeat_section #post}
+        } else {
+            quote!{#repeat_section}
+        }
     }
 }
 
@@ -120,6 +220,7 @@ impl Parse for Seq {
 #[proc_macro]
 pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let seq: Seq = parse_macro_input!(input as Seq);
+    // panic!("{:?}", seq.tokens);
 
     let code = seq.quote();
 
